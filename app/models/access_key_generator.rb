80cbd38d89be0a9c78b4f0d2bf3c64c1edbbb024
Kevin Murphy <murphy.kevin.mail@gmail.com>
2019-09-18 16:36:34 -0400

    Ask the accessor to generate its access key
    With the logic about how to generate an access key separated, this now
    changes the public API of the `AccessKeyGenerator` to accept an instance
    of the class that needs an access key generated and calls the
    appropriate method to do so.
    
    The tests have been modified to pass. This should then, with adequate
    test coverage in place, signal on a test run every place that the usage
    of this class needs to change, because the signature has changed
    significantly. The work to change the calling of this method can be done
    at this point, and we can stop here, or we can use this as a signal for
    every place where we can replace this class entirely by instead calling
    the model's `generate_access_key` method directly.

--- app/models/access_key_generator.rb
+++ app/models/access_key_generator.rb
@@ -1,12 +1,8 @@
 class AccessKeyGenerator
-  def access_key(accessor_type:, acquired_company: nil, user_id: nil)
-    if accessor_type == :company
-      Company.new(acquisition: acquired_company).generate_access_key
-    elsif accessor_type == :user
-      User.new(id: user_id).generate_access_key
-    else
-      raise UnknownAccessorType
-    end
+  def access_key(accessor)
+    accessor.generate_access_key
+  rescue NoMethodError
+    raise UnknownAccessorType
   end
 end
 
--- spec/models/access_key_generator_spec.rb
+++ spec/models/access_key_generator_spec.rb
@@ -3,9 +3,10 @@ require "rails_helper"
 RSpec.describe AccessKeyGenerator do
   describe "#access_key" do
     it "provides a base-64 encoded key if the key is for an acquired company" do
+      accessor = Company.new(acquisition: true)
       generator = AccessKeyGenerator.new
 
-      key = generator.access_key(accessor_type: :company, acquired_company: true)
+      key = generator.access_key(accessor)
 
       expect(key).to eq Base64.strict_encode64(Base64.decode64(key))
     end
@@ -13,8 +14,9 @@ RSpec.describe AccessKeyGenerator do
     it "creates an ivory tower company access key if it's a company not acquired" do
       company_regex = /CO-[0-9a-fA-F]{8}/
 
+      accessor = Company.new(acquisition: false)
       generator = AccessKeyGenerator.new
-      key = generator.access_key(accessor_type: :company, acquired_company: false)
+      key = generator.access_key(accessor)
 
       expect(key.match?(company_regex)).to eq true
     end
@@ -22,8 +24,9 @@ RSpec.describe AccessKeyGenerator do
     it "does not provide a full UUID if the key is for a company" do
       uuid_regex = /[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/
 
+      accessor = Company.new(acquisition: false)
       generator = AccessKeyGenerator.new
-      key = generator.access_key(accessor_type: :company)
+      key = generator.access_key(accessor)
 
       expect(key.match?(uuid_regex)).not_to eq true
     end
@@ -31,16 +34,19 @@ RSpec.describe AccessKeyGenerator do
     it "provides a UUID if the key is for a user with an even id" do
       uuid_regex = /[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/
 
+      accessor = User.new(id: 2)
       generator = AccessKeyGenerator.new
-      key = generator.access_key(accessor_type: :user, user_id: 2)
+
+      key = generator.access_key(accessor)
 
       expect(key.match?(uuid_regex)).to eq true
     end
 
     it "appends the user id if the user's id is odd" do
+      accessor = User.new(id: 1)
       generator = AccessKeyGenerator.new
 
-      key = generator.access_key(accessor_type: :user, user_id: 1)
+      key = generator.access_key(accessor)
 
       expect(key).to end_with("-1")
     end
@@ -48,16 +54,19 @@ RSpec.describe AccessKeyGenerator do
     it "does not provide a company key style if the key is for a user" do
       company_regex = /CO-[0-9a-fA-F]{8}/
 
+      accessor = User.new(id: 1)
       generator = AccessKeyGenerator.new
-      key = generator.access_key(accessor_type: :user, user_id: 0)
+
+      key = generator.access_key(accessor)
 
       expect(key.match?(company_regex)).not_to eq true
     end
 
     it "raises an exception if it doesn't understand the accessor type" do
+      accessor = Moon.new
       generator = AccessKeyGenerator.new
 
-      expect { generator.access_key(accessor_type: :foo) }
+      expect { generator.access_key(accessor) }
         .to raise_error UnknownAccessorType
     end
   end
