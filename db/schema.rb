eacee15bd26fbc31248456e4d3a600f48b7b27dc
Kevin Murphy <murphy.kevin.mail@gmail.com>
2019-09-18 11:35:51 -0400

    Add User
    This creates a user model to keep track of people in the system.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_18_152911) do
+ActiveRecord::Schema.define(version: 2019_09_18_153120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 2019_09_18_152911) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "acquisition", default: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
