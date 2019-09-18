93ef1b8260d4d38d44568ec524f6616ea8bfb121
Kevin Murphy <murphy.kevin.mail@gmail.com>
2019-09-18 13:35:47 -0400

    Explicitly state regexes in relevant examples
    Rather than extracting the regex defining a company or user access key
    in the tests that check to see the keys determined look as they should,
    this defines the regex multiple times within the examples that need it.
    
    This makes it more clear within the relevant example to make it obvious
    what the expectation is that it should be matching. While this does mean
    that the same code is written multiple times, and it may need to be
    updated should it change over time in different places, it adds
    explanatory value and doesn't require a person to navigate around a file
    to find the value.

--- spec/models/access_key_generator_spec.rb
+++ spec/models/access_key_generator_spec.rb
@@ -1,9 +1,6 @@
 require "rails_helper"
 
 RSpec.describe AccessKeyGenerator do
-  UUID_REGEX = /[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/
-  COMPANY_REGEX = /CO-[0-9a-fA-F]{8}/
-
   describe "#access_key" do
     it "provides a base-64 encoded key if the key is for an acquired company" do
       generator = AccessKeyGenerator.new
@@ -14,27 +11,30 @@ RSpec.describe AccessKeyGenerator do
     end
 
     it "creates an ivory tower company access key if it's a company not acquired" do
-      generator = AccessKeyGenerator.new
+      company_regex = /CO-[0-9a-fA-F]{8}/
 
+      generator = AccessKeyGenerator.new
       key = generator.access_key(accessor_type: :company, acquired_company: false)
 
-      expect(key.match?(COMPANY_REGEX)).to eq true
+      expect(key.match?(company_regex)).to eq true
     end
 
     it "does not provide a full UUID if the key is for a company" do
-      generator = AccessKeyGenerator.new
+      uuid_regex = /[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/
 
+      generator = AccessKeyGenerator.new
       key = generator.access_key(accessor_type: :company)
 
-      expect(key.match?(UUID_REGEX)).not_to eq true
+      expect(key.match?(uuid_regex)).not_to eq true
     end
 
     it "provides a UUID if the key is for a user with an even id" do
-      generator = AccessKeyGenerator.new
+      uuid_regex = /[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}/
 
+      generator = AccessKeyGenerator.new
       key = generator.access_key(accessor_type: :user, user_id: 2)
 
-      expect(key.match?(UUID_REGEX)).to eq true
+      expect(key.match?(uuid_regex)).to eq true
     end
 
     it "appends the user id if the user's id is odd" do
@@ -46,11 +46,12 @@ RSpec.describe AccessKeyGenerator do
     end
 
     it "does not provide a company key style if the key is for a user" do
-      generator = AccessKeyGenerator.new
+      company_regex = /CO-[0-9a-fA-F]{8}/
 
+      generator = AccessKeyGenerator.new
       key = generator.access_key(accessor_type: :user, user_id: 0)
 
-      expect(key.match?(COMPANY_REGEX)).not_to eq true
+      expect(key.match?(company_regex)).not_to eq true
     end
 
     it "raises an exception if it doesn't understand the accessor type" do
