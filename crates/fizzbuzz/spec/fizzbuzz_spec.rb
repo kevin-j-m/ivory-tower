aa6bbdead3870f7c0a72501374204171d0525990
Kevin Murphy <murphy.kevin.mail@gmail.com>
2019-09-22 21:02:26 -0400

    Fizz divisible by 3

--- crates/fizzbuzz/spec/fizzbuzz_spec.rb
+++ crates/fizzbuzz/spec/fizzbuzz_spec.rb
@@ -4,4 +4,8 @@ describe "Fizzbuzz" do
   it "provides a collection with the element if only counting to 1" do
     expect(Fizzbuzz.generate(1)).to eq ["1"]
   end
+
+  it "replaces the number with Fizz if it's divisible by 3" do
+    expect(Fizzbuzz.generate(3)).to eq ["1", "2", "Fizz"]
+  end
 end
--- crates/fizzbuzz/src/lib.rs
+++ crates/fizzbuzz/src/lib.rs
@@ -6,7 +6,8 @@ ruby! {
         def generate(bound: i32) -> Vec<String> {
             let mut results = Vec::new();
             for x in 1..(bound+1) {
-                results.push(x.to_string());
+                if x % 3 == 0 { results.push("Fizz".to_string()) }
+                else { results.push(x.to_string()) }
             }
             results
         }
