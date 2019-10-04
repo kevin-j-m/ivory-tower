9aedd4c0339f3ad90a1d9c550642b5b2e49df596
Kevin Murphy <murphy.kevin.mail@gmail.com>
2019-10-04 17:17:23 -0400

    remove copmany access key generation

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,11 +1,3 @@
 class Company < ApplicationRecord
   validates :name, presence: :true
-
-  def generate_access_key
-    if acquisition?
-      SecureRandom.base64
-    else
-      "CO-#{SecureRandom.hex(8)}"
-    end
-  end
 end
